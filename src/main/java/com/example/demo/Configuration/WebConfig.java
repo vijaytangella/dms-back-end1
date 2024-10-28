eb4284d76b4c824750c99fa4b333bd6ec1f3f587
Tangella Vijay <vijaytangella179@gmail.com>
2024-10-28 18:08:40 +0530

    Your commit message

--- src/main/java/com/example/demo/Configuration/WebConfig.java
+++ src/main/java/com/example/demo/Configuration/WebConfig.java
@@ -1,19 +1,17 @@
 package com.example.demo.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Adjust this path as needed
-                .allowedOrigins(
-                    "https://dms-back-end1-production.up.railway.app"
-                 
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true);
+        registry.addMapping("/**")
+                .allowedOrigins("https://dm-front-code-project-odqobjsmz-vijays-projects-b5a44689.vercel.app",
+                        "https://dm-front-code-project.vercel.app") // Update this with your frontend URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
